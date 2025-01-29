6758984b276abac0416961094175b81d4174371a
raulsb <rsorianobravo@gmail.com>
2025-01-29 09:54:40 +0100

    [ADD] aDded fields of title and description

--- src/components/Forms/ItemForm.jsx
+++ src/components/Forms/ItemForm.jsx
@@ -4,8 +4,16 @@ const ItemForm = () => {
   return (
     <div>ItemForm
         <form className='ItemForm'>ItemForm
-        <input type="text" className='item-input' placeholder='Next Item'>
+        <br></br>
+        <input type="text" className='item-input' placeholder='Item Number'>
         </input>
+        <br></br>
+         <input type="text" className='item-input' placeholder='Title'>
+        </input>
+        <br></br>
+        <input type="text" className='item-input' placeholder='Description'>
+        </input>
+        <br></br>
         <button type="submit" className='item-btn'>Add Item</button>
         </form>
     </div>
