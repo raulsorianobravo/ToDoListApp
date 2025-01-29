f1f84353bb10174976d554d1c5f40dc6a0dcef49
raulsb <rsorianobravo@gmail.com>
2025-01-29 18:05:52 +0100

    [ADD] add edit method

--- src/components/Containers/ItemManagerController.jsx
+++ src/components/Containers/ItemManagerController.jsx
@@ -8,39 +8,111 @@ uuidv4()
 
 const ItemManagerController = () => {
     
-   const [listItems, setListItems ] = useState([])
+  //=========================================================================
+  const [listItems, setListItems ] = useState([])
 
-   const api = import.meta.env.VITE_APP_API_URI
-   //console.log(api)
+  //=========================================================================
 
-   const addItem = (id, title, description) => {
-    console.log("received:", id,title,description)    
-    setListItems([...listItems, {uid: uuidv4(), id:id, title: title , description: description, createAt:(new Date()).toJSON(), isCompleted:false, isEditing:false}])
-        console.log(listItems)
-    }
+  const api = import.meta.env.VITE_APP_API_URI
+   
+  //console.log(api)
+
+  const addItem = async (id, title, description) => {
+      console.log("received:", id,title,description)    
+      const todo = {
+        "id": id,
+        "title": title,
+        "description": description,
+        "isCompleted": false,
+        "createdAt": (new Date()).toJSON()
+      }
+      const dataItem = {uid: uuidv4(), id:id, title: title , description: description, createAt:(new Date()).toJSON(), isCompleted:false, isEditing:false}
     
-    const toggleComplete = id =>{
-        setListItems(listItems.map(item => item.id === id ? {...item, isCompleted: !item.isCompleted}:item))
-        console.log(listItems)
+      //--- TODO hardcode!
+      const res = await fetch('https://localhost:7119/api/ToDoList', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(todo)
+      })
+      const dataList = await res.json()
+
+      setListItems([...listItems, {uid: uuidv4(), id:id, title: title , description: description, createAt:(new Date()).toJSON(), isCompleted:false, isEditing:false}])
+          console.log(listItems)
+  }
+
+  //=========================================================================
+    
+  const toggleComplete = id =>{
+      setListItems(listItems.map(item => item.id === id ? {...item, isCompleted: !item.isCompleted}:item))
+      console.log(listItems)
+  }
+  //=========================================================================
+  
+  const deleteItem = async id => {
+    const todo = {
+      "id": id,
+      "title": "-",
+      "description": "-",
+      "isCompleted": "-",
+      "createdAt": "-"
+    }
+    console.log(id)
+    const idTemp = id
+    //--- TODO hardcode!
+    const res = await fetch(`https://localhost:7119/api/ToDoList/${id}`, {
+        method: 'DELETE'
+    })
+    //.then(res => res.text()) // or res.json()
+    //.then(res => console.log(res))
+    //const dataList = res.json()
+    const res2 = await fetch('https://localhost:7119/api/ToDoList')
+    const dataList = await res2.json()
+        
+    setListItems(listItems.filter(item => item.id !=id))
+  } 
+
+  //=========================================================================
+  
+  const editItem = id => {
+      setListItems(listItems.map(item => item.id === id ? {...item, isEditing: !item.isEditing}:item))
+      console.log(listItems)
+
+  }
+
+  //=========================================================================
+  
+  const editItemForm = async (itemid, itemTitle, ItemDescription, id) => {
+      console.log(itemid, itemTitle, ItemDescription, id)
+      
+      const item = {
+        "id": id,
+        "title": itemTitle,
+        "description": ItemDescription,
+        "isCompleted": false,
+        "createdAt": addItem.createdAt
     }
 
-    const deleteItem = id => {
-        setListItems(listItems.filter(item => item.id !=id))
-      }
+    console.log(id)
+    const idTemp = id
+    const res = await fetch(`https://localhost:7119/api/ToDoList/${id}`, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(item)
+        })
+    
+    //.then(res => res.text()) // or res.json()
+    //.then(res => console.log(res))
+    //const dataList = res.json()
+    const resGet = await fetch('https://localhost:7119/api/ToDoList')
+    const dataList = await resGet.json()
+    setListItems([...dataList]);  
+    //setListItems(listItems.map(item => item.id === id ? {...item, id:itemid, title:itemTitle, description:ItemDescription, isEditing: !item.isEditing}:item))
+  }
 
-      const editItem = id => {
-        setListItems(listItems.map(item => item.id === id ? {...item, isEditing: !item.isEditing}:item))
-        console.log(listItems)
-
-      }
-
-      const editItemForm = (itemid, itemTitle, ItemDescription, id) => {
-        console.log(itemid, itemTitle, ItemDescription)
-        setListItems(listItems.map(item => item.id === id ? {...item, id:itemid, title:itemTitle, description:ItemDescription, isEditing: !item.isEditing}:item))
-      }
-
-          //--- Update the list at evey change. Not Forced the update
-    useEffect(() => {
+  //=========================================================================
+  
+  //--- Update the list at evey change. Not Forced the update
+  useEffect(() => {
       const LoadDataAPI = async () => {
         //const res = await fetch('https://localhost:7119/api/ToDoList')
         const res = await fetch(api)
@@ -50,7 +122,9 @@ const ItemManagerController = () => {
       LoadDataAPI()
     }, [])
 
-    return (
+  //=========================================================================
+  
+  return (
         <div className='ItemManagerController'>
         ItemManagerController
         <ItemForm addItem={addItem} />
