b1e8f99fa7b68aac571e73c3cd78041b926d4e74
raulsb <rsorianobravo@gmail.com>
2025-01-29 10:06:11 +0100

    [ADD] added onchange methods and useStates

--- src/components/Forms/ItemForm.jsx
+++ src/components/Forms/ItemForm.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ItemForm = () => {
-  return (
+    
+    //===============================================================
+    const [id, setID] = useState("")
+    const [title,setTitle] = useState("")
+    const [description, setDescription] = useState("")
+
+    //===============================================================
+    const handleChangeId = e => {
+        setID(e.target.value)
+        //console.log(e.target.value)
+        console.log(id)
+    }
+    //===============================================================
+    const handleChangeTitle =  e => {
+        setTitle(e.target.value)
+        //console.log(e.target.value)
+        console.log(title)
+    }
+    //===============================================================
+    const handleChangeDescription =  e => {
+        setDescription(e.target.value)
+        //console.log(e.target.value)
+        console.log(description)
+    }
+  
+    //===============================================================
+  
+    return (
     <div>ItemForm
         <form className='ItemForm'>ItemForm
         <br></br>
-        <input type="text" className='item-input' placeholder='Item Number'>
+        <input type="text" className='item-input' placeholder='Item Number' onChange={handleChangeId}>
         </input>
         <br></br>
-         <input type="text" className='item-input' placeholder='Title'>
+         <input type="text" className='item-input' placeholder='Title' onChange={handleChangeTitle}>
         </input>
         <br></br>
-        <input type="text" className='item-input' placeholder='Description'>
+        <input type="text" className='item-input' placeholder='Description' on onChange={handleChangeDescription}>
         </input>
         <br></br>
         <button type="submit" className='item-btn'>Add Item</button>
