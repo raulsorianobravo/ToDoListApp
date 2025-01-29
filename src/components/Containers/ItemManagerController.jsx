cf8e72e306ff6c1342ad01258f84fbb4835d08aa
raulsb <rsorianobravo@gmail.com>
2025-01-29 10:41:21 +0100

    [ADD] process the return of the submit

--- src/components/Containers/ItemManagerController.jsx
+++ src/components/Containers/ItemManagerController.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ItemForm from '../Forms/ItemForm'
+import {v4 as uuidv4} from 'uuid'
+
+uuidv4()
 
 const ItemManagerController = () => {
-  return (
-    <div className='ItemManagerController'>
+    
+   const [listItems, setListItems ] = useState([])
+
+   const addItem = (id, title, description) => {
+    console.log("received:", id,title,description)    
+    setListItems([...listItems, {uid: uuidv4(), id:id, title: title , description: description, createAt:(new Date()).toJSON(), isCompleted:false, isEditing:false}])
+        console.log(listItems)
+    } 
+
+    return (
+        <div className='ItemManagerController'>
         ItemManagerController
-        <ItemForm />
+        <ItemForm addItem={addItem} />
     </div>
   )
 }
--- src/components/Forms/ItemForm.jsx
+++ src/components/Forms/ItemForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 
-const ItemForm = () => {
+const ItemForm = props => {
     
+    const {addItem} = props
+
     //===============================================================
     const [id, setID] = useState("")
     const [title,setTitle] = useState("")
@@ -29,6 +31,10 @@ const ItemForm = () => {
     const handleSubmit = e => {
         e.preventDefault()
         console.log(id, title, description)
+        addItem(id, title, description)
+        setID("")
+        setTitle("")
+        setDescription("")
     }
     //===============================================================
   
@@ -36,13 +42,13 @@ const ItemForm = () => {
     <div>ItemForm
         <form className='ItemForm' onSubmit={handleSubmit}>ItemForm
         <br></br>
-        <input type="text" className='item-input' placeholder='Item Number' onChange={handleChangeId}>
+        <input type="text" className='item-input' value={id} placeholder='Item Number' onChange={handleChangeId}>
         </input>
         <br></br>
-         <input type="text" className='item-input' placeholder='Title' onChange={handleChangeTitle}>
+         <input type="text" className='item-input' value={title} placeholder='Title' onChange={handleChangeTitle}>
         </input>
         <br></br>
-        <input type="text" className='item-input' placeholder='Description' on onChange={handleChangeDescription}>
+        <input type="text" className='item-input' value={description} placeholder='Description' on onChange={handleChangeDescription}>
         </input>
         <br></br>
         <button type="submit" className='item-btn'>Add Item</button>
