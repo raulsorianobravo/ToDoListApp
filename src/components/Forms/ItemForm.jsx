c452a60d4923507930a604573c1dbd603d5d48f0
raulsb <rsorianobravo@gmail.com>
2025-01-29 09:50:35 +0100

    [ADD] Fom with text input and button

--- src/components/Forms/ItemForm.jsx
+++ src/components/Forms/ItemForm.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ItemForm = () => {
   return (
-    <div>ItemForm</div>
+    <div>ItemForm
+        <form className='ItemForm'>ItemForm
+        <input type="text" className='item-input' placeholder='Next Item'>
+        </input>
+        <button type="submit" className='item-btn'>Add Item</button>
+        </form>
+    </div>
   )
 }
 
