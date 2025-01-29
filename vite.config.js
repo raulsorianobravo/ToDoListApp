1a2647510c3cfbb274e9f203a9dc93686233a731
raulsb <rsorianobravo@gmail.com>
2025-01-29 12:58:04 +0100

    [MOD] Change port server

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //change port for production
+  preview: {
+    port: 3001,
+  },
+// for dev
+  server: {
+    port: 3000,
+  },
 })
